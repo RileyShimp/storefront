74bdbd52a6a3e420e1a136acd759c12113998c3a
Riley Shimp <rilryshimp@gmail.com>
2022-05-25 15:14:22 -0700

    Add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav className='nav'>
+      <a href='/' className="site-title">Store</a>
+      <ul>
+        <li>
+          <a href='/pricing'>Pricing</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
