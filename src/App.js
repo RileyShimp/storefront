b4fb399bd563a89b59ab7c8aaa5cbc5195299e14
Riley Shimp <rilryshimp@gmail.com>
2022-05-25 19:16:04 -0700

    Add pages for each link in navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Navbar from './Navbar';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home';
+import About from './pages/About';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
+
+  let Component;
+
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home 
+      break
+    case "/pricing":
+      Component = Pricing 
+      break
+    case "/about":
+      Component = About
+      break
+  }
+
   return (
     <div className="App">
+      <>
       <Navbar/>
+      <div className='container'>
+      <Component/>
+      </div>
+      </>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,22 @@
 export default function Navbar() {
   return (
     <nav className='nav'>
-      <a href='/' className="site-title">Store</a>
+      <a href='/' className="site-title">
+        Store
+      </a>
       <ul>
-        <li>
-          <a href='/pricing'>Pricing</a>
-        </li>
-        <li>
-          <a href='/about'>About</a>
-        </li>
+        <CustomLink href='/pricing'>Pricing</CustomLink>
+        <CustomLink href='/about'>About</CustomLink>
       </ul>
     </nav>
   );
+}
+
+function CustomLink ({ href, children, ...props}) {
+  const path = window.location.pathname;
+  return (
+  <li className={path === href ? "active" : ""}>
+    <a href={href} {...props}>{children}</a>
+  </li>
+  )
 }
