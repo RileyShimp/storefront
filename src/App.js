dca0ddd1fd347a43ece10ae32598c798c353a652
Riley Shimp <rilryshimp@gmail.com>
2022-05-25 19:29:21 -0700

    Refactor to use react router

--- src/App.js
+++ src/App.js
@@ -7,26 +7,16 @@ import {Route, Routes} from "react-router-dom";
 
 function App() {
 
-  let Component;
-
-  switch (window.location.pathname) {
-    case "/":
-      Component = Home 
-      break
-    case "/pricing":
-      Component = Pricing 
-      break
-    case "/about":
-      Component = About
-      break
-  }
-
   return (
     <div className="App">
       <>
       <Navbar/>
       <div className='container'>
-      <Component/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/pricing' element={<Pricing/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
       </div>
       </>
     </div>
